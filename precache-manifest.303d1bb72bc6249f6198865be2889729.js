7ce27afd4232f31752cf7c1f4331d3437786a5fa
= <=>
2019-11-19 09:06:57 +0700

    my first comment

---
+++ precache-manifest.303d1bb72bc6249f6198865be2889729.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c57e1147a8dd127dfe",
+    "url": "/css/about.ed036f12.css"
+  },
+  {
+    "revision": "67dc340e52e7b8c9f0ca",
+    "url": "/css/chunk-vendors.5b03080e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e49a3ac384de5324f5a0fd991f433d4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69c57e1147a8dd127dfe",
+    "url": "/js/about.294c8623.js"
+  },
+  {
+    "revision": "cee694ac5cbfbc329527",
+    "url": "/js/app.e8dc13f5.js"
+  },
+  {
+    "revision": "67dc340e52e7b8c9f0ca",
+    "url": "/js/chunk-vendors.714255cc.js"
+  },
+  {
+    "revision": "0d652e21a52a98e5cc24699569fb5051",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
